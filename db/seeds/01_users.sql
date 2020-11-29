14f7918d15a564e962f91dbc09126c5c4297bb56
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-29 15:42:32 +0000

    User seed data updataed with hashed password password

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,10 +1,10 @@
 -- Users table seeds here (Example)
 INSERT INTO users (handle, email, password, avatar_url)
-VALUES ('Tiny_Emperor', 'myemail@gmail.com', 'password', '');
+VALUES ('Tiny_Emperor', 'myemail@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '');
 
 INSERT INTO users (handle, email, password, avatar_url)
-VALUES ('Ground Control', 'houstonwehaveaproblem@gmail.com', 'password', '');
+VALUES ('Ground Control', 'houstonwehaveaproblem@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '');
 
 INSERT INTO users (handle, email, password, avatar_url)
-VALUES ('Drake', 'drake@gmail.com', 'password', '');
+VALUES ('Drake', 'drake@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '');
 
