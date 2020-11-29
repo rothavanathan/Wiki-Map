06a545aca3cb2c89f749baf76502bfed55ec2a6a
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-29 02:26:56 +0000

    markers seeds are attached to map_id 4 drizzys clubs

--- db/seeds/03_markers.sql
+++ db/seeds/03_markers.sql
@@ -1,16 +1,16 @@
 --markers for 'drizzy's clubs'
 
 INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
-VALUES (3, '{ lat: 43.65785712314121,  lng: -79.40664101589161 }', 'El Ranchos', 'the tequila be flowin over at el ranchos', 'https://www.pexels.com/photo/artists-audience-band-blue-426976/', 'A crowd dancing at El Ranchos nightclub' );
+VALUES (4, '{ lat: 43.65785712314121,  lng: -79.40664101589161 }', 'El Ranchos', 'the tequila be flowin over at el ranchos', 'https://www.pexels.com/photo/artists-audience-band-blue-426976/', 'A crowd dancing at El Ranchos nightclub' );
 
 INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
-VALUES (3, '{ lat: 43.65785712314121,  lng: -79.40664101589161 }', 'BLND TGER', 'Lose your goddamn mind and your eyes', 'https://www.pexels.com/photo/people-dancing-inside-dim-room-2114365/', 'A crowd dancing at El Ranchos nightclub' );
+VALUES (4, '{ lat: 43.65785712314121,  lng: -79.40664101589161 }', 'BLND TGER', 'Lose your goddamn mind and your eyes', 'https://www.pexels.com/photo/people-dancing-inside-dim-room-2114365/', 'A crowd dancing at El Ranchos nightclub' );
 
 INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
-VALUES (3, '{ lat: 43.65774994467593, lng: -79.4002035011423 }', 'El Mocambo', 'Live Music Venue', 'https://www.pexels.com/photo/neon-light-signages-on-wall-1426620/', 'A punk band at El Mocambo' );
+VALUES (4, '{ lat: 43.65774994467593, lng: -79.4002035011423 }', 'El Mocambo', 'Live Music Venue', 'https://www.pexels.com/photo/neon-light-signages-on-wall-1426620/', 'A punk band at El Mocambo' );
 
 INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
-VALUES (3, '{ lat: 43.64926425834702, lng: -79.3959058299788 }', 'Horseshoe Tavern', 'a small bard for upcoming music', 'https://www.pexels.com/photo/fans-filming-concert-on-smartphones-in-night-club-4319117/e', 'A shitty local band playing at the Horseshow' );
+VALUES (4, '{ lat: 43.64926425834702, lng: -79.3959058299788 }', 'Horseshoe Tavern', 'a small bard for upcoming music', 'https://www.pexels.com/photo/fans-filming-concert-on-smartphones-in-night-club-4319117/e', 'A shitty local band playing at the Horseshow' );
 
 
 
