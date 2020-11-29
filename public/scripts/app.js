405afd9ca9767ede9a031ea2a5d5ba8537ed9b80
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-29 00:41:13 +0000

    Users, maps, map_permissions, and marker schemas added to schema/01_users.sql

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,39 +13,39 @@
 const markers = [
   {
     id: 1,
+    map_id: "",
     latlng: { lat: 43.65785712314121,  lng: -79.40664101589161 },
     title: 'El Ranchos',
     description: "Night Club",
     image_url: "https://www.pexels.com/photo/artists-audience-band-blue-426976/",
     image_alt_text: "A crowd dancing at El Ranchos nightclub",
-    map_id: "",
   },
   {
     id: 2,
+    map_id: "",
     latlng: { lat: 43.65562780833972, lng: -79.41236028181297 },
     title: 'BLND TGER',
     description: "Night Club",
     image_url: "https://www.pexels.com/photo/people-dancing-inside-dim-room-2114365/",
     image_alt_text: "A crowd dancing at BLNG TGER nightclub",
-    map_id: "",
   },
   {
     id: 3,
+    map_id: "",
     latlng: { lat: 43.65774994467593, lng: -79.4002035011423 },
     title: 'El Mocambo',
     description: "Live Music Venue",
     image_url: "https://www.pexels.com/photo/neon-light-signages-on-wall-1426620/",
     image_alt_text: "A punk band at El Mocambo",
-    map_id: "",
   },
   {
     id: 4,
+    map_id: "",
     latlng: { lat: 43.64926425834702, lng: -79.3959058299788 },
     title: 'Horseshoe Tavern',
     description: "Live Music Venue",
     image_url: "https://www.pexels.com/photo/fans-filming-concert-on-smartphones-in-night-club-4319117/e",
     image_alt_text: "A shitty local band playing at the Horseshow",
-    map_id: "",
   }
 ];
 
