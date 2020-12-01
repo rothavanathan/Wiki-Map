6fb672d7a5d8c987689d66f64b7c081cce0ea4be
Kelvin <tobar411@gmail.com>
2020-12-01 19:17:20 +0000

    hid profile stuff

---
+++ public/scripts/profile.js
@@ -0,0 +1,14 @@
+const loadProfile = (user)=> {
+  $('.sidebar-heading')
+  .append('<div class="avatar"><img src="https://www.flaticon.com/svg/static/icons/svg/147/147144.svg"></div>')
+
+  $('.profileTabs').show();
+}
+// .prepend('<div><img class="https://www.flaticon.com/svg/static/icons/svg/147/147144.svg"></div>')
+// }
+
+$(document).ready(() => {
+  $('.profileTabs').hide();
+  console.log(document)
+  loadProfile();
+});
