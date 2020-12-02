c19ed30599613217e7fd735ad4a67a82c0894c72
Sebastian <lynchseb@gmail.com>
2020-12-02 19:46:00 +0000

    built save map routes

--- routes/maps.js
+++ routes/maps.js
@@ -18,8 +18,8 @@ module.exports = (db) => {
     `;
     console.log(query);
     return db.query(query)
-      .then(data => {
-        const maps = data.rows;
+      .then(mapD => {
+        const maps = mapD.rows;
         res.json({ maps });
       })
       .catch(err => {
@@ -44,8 +44,8 @@ module.exports = (db) => {
     `;
     //db query should use cookies for user id
     db.query(query, [req.session.userId])
-      .then(data => {
-        maps = data.rows;
+      .then(mapD => {
+        maps = mapD.rows;
         res.json({ maps });
       })
       .catch(err => {
@@ -63,8 +63,8 @@ module.exports = (db) => {
     WHERE markers.map_id = $1
     `;
     db.query(query, [req.params.id])
-      .then(data => {
-        maps = data.rows;
+      .then(mapD => {
+        maps = mapD.rows;
         res.json({ maps });
       })
       .catch(err => {
@@ -73,5 +73,42 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.post("/save", (req, res) => {
+    console.log("this happened")
+    const owner_id = req.session.userId;
+    console.log("owner_id", owner_id)
+    console.log("this is req.body", req.body)
+    const mapData = req.body;
+    console.log(mapData)
+    // res.send(mapFormData)
+    const query1 = `
+    INSERT INTO maps (owner_id, title, description, thumbnail_photo_url, thumbnail_alt_text, isPublic)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING id
+    `;
+
+    const params1 = [owner_id, mapData.title, mapData.description, mapData.thumbnail_photo_url, mapData.thumbnail_alt_text, mapData.isPublic];
+    db.query(query1, params1)
+    .then(data => {
+      map_id = data.rows[0]
+      // console.log(mapD.rows)
+      res.send(map_id)
+      // map_id = data.rows[0].id
+      // const params = [mapData.user_id, map_id, true];
+    })
+    .catch(err => {
+      console.log(err)
+    });
+
+  })
+
+
   return router;
 };
+
+// const query2 = `
+//     INSERT INTO map_permissions (user_id, map_id, isAuthenticated)
+//     VALUES ($1, $2, $3)
+//     RETURNING id
+//     `;
