f766b2df87888fe57a40ab70341581b545408aa1
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-12-04 13:57:45 +0000

    Extra seed data added - two more maps by Drake and map_permissions associated with them

--- db/seeds/02_maps_and_permissions.sql
+++ db/seeds/02_maps_and_permissions.sql
@@ -1,16 +1,28 @@
 -- Maps table seeds
+
+-- maps.id 1
 INSERT INTO maps (owner_id, title, description, thumbnail_photo_url, thumbnail_alt_text, isPublic)
 VALUES (1, 'Hikes in Newfoundland', 'The most scenic hikes I know of from around the province' ,'https://images.pexels.com/photos/2629320/pexels-photo-2629320.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', 'A scenic photo of Gros Morne National park', true);
 
+-- maps.id 2
 INSERT INTO maps (owner_id, title, description, thumbnail_photo_url, thumbnail_alt_text, isPublic)
 VALUES (1, 'Restaurants in St. Johns', 'The best places to grab a bite.', 'https://images.pexels.com/photos/1528013/pexels-photo-1528013.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', 'Gorgeous brunch at Mallard Cottage', true);
 
+-- maps.id 3
 INSERT INTO maps (owner_id, title, description, thumbnail_photo_url, thumbnail_alt_text, isPublic)
 VALUES (2, 'Views of the Stars', 'Remote places in Ontario for crystal clear star gazing', 'https://images.pexels.com/photos/1624438/pexels-photo-1624438.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', 'Clear view of the celestial night sky', true);
 
+-- maps.id 4
 INSERT INTO maps (owner_id, title, description, thumbnail_photo_url, thumbnail_alt_text, isPublic)
-VALUES (3, 'Drizzys Clubs', 'Drakes personal hot spots for a night out in the 6', 'https://images.pexels.com/photos/3394221/pexels-photo-3394221.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', 'Inside peek at a VIP club', true);
+VALUES (3, 'Drizzys Clubs', 'Drake''s personal hot spots for a night out in the 6', 'https://images.pexels.com/photos/3394221/pexels-photo-3394221.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', 'Inside peek at a VIP club', true);
 
+-- maps.id 5
+INSERT INTO maps (owner_id, title, description, thumbnail_photo_url, thumbnail_alt_text, isPublic)
+VALUES (3, 'Drizzys Eats', 'Where Drake wines and dines in T.O.', 'https://images.pexels.com/photos/776538/pexels-photo-776538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'High end gastropub', true);
+
+-- maps.id 6
+INSERT INTO maps (owner_id, title, description, thumbnail_photo_url, thumbnail_alt_text, isPublic)
+VALUES (3, 'Drizzys Sexy Date Spots', 'For those steamy date nights', 'https://images.unsplash.com/photo-1577183732813-63b3ac4c2f9a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzJ8fHZpcCUyMGNsdWJ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', 'Private getaways', false);
 
 -- maps_permissions table seeds
 
@@ -26,6 +38,12 @@ VALUES (1, 3, true, true, false);
 INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
 VALUES (1, 4, true, false, false);
 
+INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
+VALUES (1, 5, true, false, false);
+
+INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
+VALUES (1, 6, false, false, false);
+
   --user.id 2: Ground Control's favorite maps (1, 4)
 
 INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
@@ -40,6 +58,12 @@ VALUES (2, 3, false, true, true);
 INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
 VALUES (2, 4, true, false, false);
 
+INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
+VALUES (2, 5, false, false, false);
+
+INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
+VALUES (2, 6, false, false, false);
+
 --user.id 3: Drake's favorite maps (1, 4)
 
 INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
@@ -53,3 +77,9 @@ VALUES (3, 3, false, true, true);
 
 INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
 VALUES (3, 4, false, false, false);
+
+INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
+VALUES (3, 5, true, true, true);
+
+INSERT INTO map_permissions (user_id, map_id, isFavorite, isAuthenticated, isContributor)
+VALUES (3, 6, true, true, true);
--- db/seeds/03_markers.sql
+++ db/seeds/03_markers.sql
@@ -12,7 +12,7 @@ VALUES (1, '{"lat": 49.59642312054635,  "lng": -57.78418749860119}', 'Gros Morne
 --markers for MAP_2 'restaurants'
 
 INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
-VALUES (2, '{"lat": 47.581734771390416, "lng": -52.67836612804541}', 'Mallard Cottage', 'A quaint cottage tucked in Quidi Vidi village that serves a mighty brunch.', 'https://images.pexels.com/photos/426976/pexels-photo-426976.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'A king''s brunch at Mallard Cottage' );
+VALUES (2, '{"lat": 47.581734771390416, "lng": -52.67836612804541}', 'Mallard Cottage', 'A quaint cottage tucked in Quidi Vidi village that serves a mighty brunch.', 'https://images.pexels.com/photos/1528013/pexels-photo-1528013.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', 'A king''s brunch at Mallard Cottage' );
 
 INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
 VALUES (2, '{"lat": 47.5706394887136, "lng": -52.70115540106286}', 'Bannerman Brewing Co.', 'A beerhall with modern twists yet homely sensibilities. Tres hygge.', 'https://images.pexels.com/photos/4508641/pexels-photo-4508641.jpeg?cs=srgb&dl=pexels-wewe-yang-4508641.jpg&fm=jpg', 'A sophisticated beerhall.' );
@@ -44,7 +44,7 @@ VALUES (3, '{"lat": 45.620983287753845, "lng": -78.5710543790885}', 'Track and T
 
 
 
-
+--markers for MAP_4 'Drizzy's Clubs'
 
 INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
 VALUES (4, '{"lat": 43.65785712314121, "lng": -79.40664101589161}', 'El Ranchos', 'the tequila be flowin over at el ranchos', 'https://images.pexels.com/photos/426976/pexels-photo-426976.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'A crowd dancing at El Ranchos nightclub' );
@@ -59,4 +59,34 @@ INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_te
 VALUES (4, '{"lat": 43.64926425834702, "lng": -79.3959058299788}', 'Horseshoe Tavern', 'a small bar for upcoming music', 'https://images.pexels.com/photos/4319117/pexels-photo-4319117.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260', 'A shitty local band playing at the Horseshow' );
 
 
+--markers for MAP_5 'Drizzy's Eats'
+
+INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
+VALUES (5, '{"lat": 43.678382204817005,  "lng": -79.40905775972779}', 'Don Alfonso', 'Modern, chef-driven Mediterranean restaurant with à la carte & set menus, cocktails & a wine list.', 'https://images.pexels.com/photos/984888/pexels-photo-984888.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'Don Alfonso Mediterranean restaurant' );
+
+INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
+VALUES (5, '{"lat": 43.62514196997776, "lng":  -79.509961473223}', 'Sushi Kaji', 'Cozy, upscale restaurant where noted chef Mitsuhiro Kaji offers 2 tasting menus of Japanese cuisine.', 'https://images.pexels.com/photos/1310777/pexels-photo-1310777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Table full of ramen and sushi' );
+
+INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
+VALUES (5, '{"lat": 43.64501144462808,  "lng": -79.40069238671579 }', 'Buca', 'Rustic Italian fare, including housemade pastas & charcuterie, served in a buzzy, high-end setting.', 'https://images.pexels.com/photos/1579739/pexels-photo-1579739.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'The dining room at Buca' );
+
+INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
+VALUES (5, '{"lat": 43.648731217340796, "lng": -79.39596947322224}', 'Alo', 'French tasting menu served in stylish, serene surrounds atop a Victorian building.
+', 'https://images.pexels.com/photos/9315/menu-restaurant-france-eating-9315.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'A place setting at Alo' );
+
+
+--markers for MAP_6 'Drizzys Sexy Date Spots'
+
+INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
+VALUES (6, '{"lat": 43.649372057998704,  "lng": -79.38920031471349}', 'Fifth Social Club', 'Stylish ex-loft with an upscale atmosphere, hosting private events, DJs & dancing crowds.
+', 'https://images.pexels.com/photos/219095/pexels-photo-219095.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'The moody glow at the entrance of Fifth Social CLub' );
+
+INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
+VALUES (6, '{"lat": 43.64535880728066,  "lng": -79.39791304354905}', 'Early Mercy', 'Trendy cocktail bar with light eats, a deck & offbeat decor, including a word wall.', 'https://images.pexels.com/photos/1581384/pexels-photo-1581384.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'The courtyard at Early Mercy' );
+
+INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
+VALUES (6, '{"lat": 43.64396668386991, "lng": -79.40206265704259 }', 'EFS', 'Hip nightclub with leather banquettes, concrete floors & a rooftop patio, plus eclectic DJ''d tunes.', 'https://images.pexels.com/photos/1481276/pexels-photo-1481276.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'A DJ set by DangerMouse at EFS' );
+
+INSERT INTO markers (map_id, latlng, title, description, image_url, image_alt_text)
+VALUES (6, '{"lat": 43.67412593813156, "lng":  -79.39604818587749}', 'Figures', 'Comic book characters dominate the plush decor of this clubby spot serving eclectic tapas & drinks.', 'https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'The bar at Figures' );
 
